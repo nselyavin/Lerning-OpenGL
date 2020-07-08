895ce929d7c1184115a6cc218b41a48ae856e59f
Fume Fun <nselyavin@inbox.ru>
2020-07-08 16:30:11 +0300

    Урок 6 текстурки

--- OpenGl_Learn/main.cpp
+++ OpenGl_Learn/main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+#include "stb_image.h"
 #include "Shader.h"
 
 void framebufferSize(GLFWwindow* window, int width, int height); 
@@ -10,30 +11,8 @@ void processInput(GLFWwindow * window);
 const GLuint SCR_WIDTH = 800;
 const GLuint SCR_HEIGHT = 600;
 
-
-const char *vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"layout (location = 1) in vec3 aColor;\n"
-"out vec3 ourColor;\n"
-"void main()\n"
-"{\n"
-"	gl_Position = vec4(aPos, 1.0);\n"
-"	ourColor = aColor;\n"
-"}\0";
-
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 color;\n"
-"in vec3 ourColor;\n"
-"void main()\n"
-"{\n"
-"	color = vec4(ourColor, 1.0f);\n"
-"}\0";
-
-
 int main() 
 {
-	
-
 	// Инициализация и конфигурирование
 	//-------------------------------
 	glfwInit();
@@ -66,12 +45,64 @@ int main()
 	//--------------------------------
 	Shader ourShader("../ShaderPrograms/vertexShader.vert", "../ShaderPrograms/fragmentShader.frag");
 
+	// Создание текстуры 1
+	//--------------------------
+	GLuint texture1;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	int width, height, nrCompenents;
+	stbi_set_flip_vertically_on_load(true); 
+	unsigned char* data =  stbi_load("../Textures/container.jpg", &width, &height, &nrCompenents, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "Failed image load\n";
+	}
+	stbi_image_free(data);
+
+	// Загрузка текстуры 2
+	GLuint texture2;
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int width2, height2, nrComponents2;
+	// Чтобы изображение загружалось перевернутым, так как OpenGL ожидает что координата 0.0 
+	//на оси Y будет находиться в нижней части изображения, но изображения обычно имеют 0.0 в верхней части оси Y
+	data = stbi_load("../Textures/poni.jpg", &width, &height, &nrComponents2, 0);
+
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "Failed to load image\n";
+	}
+
 	// Инициализация вершин
 	//---------------------------
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
-		 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
-		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,
+		-0.7f,  0.7f, 0.0f,  1.0f, 0.0f, 0.0f,  0.0f, 1.0f,  // В-Л
+		 0.7f,  0.7f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 1.0f,  // В-П
+		 0.7f, -0.7f, 0.0f,  0.0f, 0.0f, 1.0f,	1.0f, 0.0f,  // Н-П
+		-0.7f, -0.7f, 0.0f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f   // Н-Л
+	};
+
+	GLuint indiceis[] = {
+		0, 1, 2,
+		3, 2, 0
 	};
 	
 	// Генерацию буферов
@@ -79,45 +110,67 @@ int main()
 
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	// сначала связываем объект вершинного массива, затем связываем и устанавливаем вершинный буфер(ы), и затем конфигурируем вершинный атрибут(ы).
 	glBindVertexArray(VAO);
+
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indiceis), indiceis, GL_STATIC_DRAW);
+
 	// Координатный атрибут
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	// Цветовой атрибут
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
-	// данное действие разрешено, вызов glVertexAttribPointer зарегистрировал VBO как привязанный вершинный буферный объект для вершинного атрибута, так что после мы можем спокойно отвязать
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	// Текстурный атрибут
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
-	// не отвязывайте EBO, пока VАО активен, поскольку связанного объект буфера элемента хранится в VАО; сохраняйте привязку EBO.
-	// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	ourShader.use();
+	ourShader.setInt("texture1", 0);
+	ourShader.setInt("texture2", 1);
 
-	// После этого вы можете отменить привязку VАО, чтобы другие вызовы VАО случайно не изменили этот VAO, но это редко происходит.
-	// Изменение других значений VAO требует вызова glBindVertexArray в любом случае, поэтому мы обычно не снимаем привязку VAO (или VBO), когда это непосредственно не требуется.
-	glBindVertexArray(0);
+	int invert = 0;
+	float opacity = 0.0, opacity2 = 0.0;
+	ourShader.setFloat("opacity", opacity);
+	ourShader.setFloat("opacity2", opacity2);
+	ourShader.setInt("invert", invert);
 
 	// Цикл рендера
 	//---------------------------
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 
+		srand(glfwGetTimerValue());
+		opacity = rand() % 2;
+		ourShader.setFloat("opacity", opacity);
+		if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+			invert = 1;
+			ourShader.setInt("invert", invert);
+		}
+		if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+			invert = 0;
+			ourShader.setInt("invert", invert);
+		}
+
 		glClearColor(0.3f, 0.5f, 0.2f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+
 		ourShader.use();
-		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-			ourShader.setFloat("Offset", 0.5);
-		}
-		glBindVertexArray(VAO); // поскольку у нас есть только один VАО, нет необходимости связывать его каждый раз, но мы сделаем это, чтобы все было немного более организованно
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		// glBindVertexArray(0); // не нужно каждый раз его отвязывать  
+		glBindVertexArray(VAO); 
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // Данная функция сама наложит текстуру на наш элемент
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -127,6 +180,7 @@ int main()
 	//------------------------
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
 	glfwTerminate();
 	return 0;
@@ -143,9 +197,7 @@ void framebufferSize(GLFWwindow* window, int width, int height)
 //-----------------------------
 void processInput(GLFWwindow* window)
 {
-
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
 	}
 }
-
--- ShaderPrograms/fragmentShader.frag
+++ ShaderPrograms/fragmentShader.frag
@@ -1,12 +1,20 @@
+  
 #version 330 core
+out vec4 FragColor;
 
-out vec4 color;
+in vec3 ourColor;
+in vec2 TexCoord;
 
-in vec3 ourPosition;
-//in vec3 ourColor;
+// texture sampler
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform float opacity;
+uniform float opacity2;
+uniform int invert;
 
 void main()
 {
-	color = vec4(ourPosition, 1.0f);
-
+	
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(abs(invert - TexCoord.x), TexCoord.y)), opacity);
+	//FragColor += vec4(ourColor, 1.0f) * opacity2;
 }
