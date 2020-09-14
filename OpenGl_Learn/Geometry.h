8cf5a3328477571273cfff1a8a8fe1ddc5057141
Fume <nselyavin@inbox.ru>
2020-09-14 11:00:36 +0300

    Some additions for transormation of shader

--- OpenGl_Learn/Geometry.h
+++ OpenGl_Learn/Geometry.h
@@ -16,20 +16,25 @@ protected:
 	glm::vec3 position;
 	glm::vec3 rotation;
 	glm::vec3 scale;
+	glm::mat4 transform;
 	Material* material;
 
 public:
 	Geometry() {
 		VBO = 0;
 		VAO = 0;
-		position = glm::vec3(0.0f, 0.0f, 0.0f);
-		rotation = glm::vec3(0.0f, 0.0f, 0.0f);
-		scale = glm::vec3(0.0f, 0.0f, 0.0f);
+		verticles = nullptr;
+		material = nullptr;
+		position = glm::vec3(1.0f);
+		rotation = glm::vec3(0.0f);
+		scale = glm::vec3(1.0f);
 	}
 
 	Geometry(float posx, float posy, float posz) {
 		VBO = 0;
 		VAO = 0;
+		verticles = nullptr;
+		material = nullptr;
 		position = glm::vec3(posx, posy, posz);
 		rotation = glm::vec3(0.0f, 0.0f, 0.0f);
 		scale = glm::vec3(0.0f, 0.0f, 0.0f);
@@ -44,6 +49,14 @@ public:
 	~Geometry() {
 		glDeleteVertexArrays(1, &VAO);
 		glDeleteBuffers(1, &VBO);
+	}
+
+protected:
+	void acceptTransform() {
+		transform = glm::mat4(1.0f);
+		transform = glm::translate(transform, position);
+		// ToDo преобразовать вектор3 в кватернион \\
+		transform = glm::rotate(transform, )
 	}
 };
 
